dd4efd306521615eda5e9e39945c9e6642ae3db4
CHANDAN HM <109410990+chandanhm1999@users.noreply.github.com>
2024-06-07 01:45:13 +0530

    Update page.jsx

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ProductAero from "../../components/ProductAero/ProductAero";
 
-export default function page() {
+export default function Page() {
   
 
   return (
